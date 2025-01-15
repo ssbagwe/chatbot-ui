30fd88a1f6244422ce3f70b8ec2eb127bb1d8209
Sameer Bagwe <139823155+ssbagwe@users.noreply.github.com>
2025-01-15 14:19:03 -0800

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -1,5 +1,3 @@
-import { OPENAI_API_TYPE } from '../utils/app/const';
-
 export interface OpenAIModel {
   id: string;
   name: string;
@@ -9,23 +7,17 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
+  GPT_4o = 'gpt-4o',
+  GPT_4o_latest = 'chatgpt-4o-latest',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_4o;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
@@ -36,10 +28,16 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
+  [OpenAIModelID.GPT_4o]: {
+    id: OpenAIModelID.GPT_4o,
+    name: 'GPT-4o',
+    maxLength: 24000,
+    tokenLimit: 128000,
+  },
+  [OpenAIModelID.GPT_4o_latest]: {
+    id: OpenAIModelID.GPT_4o_latest,
+    name: 'ChatGPT-4o-Latest',
+    maxLength: 24000,
+    tokenLimit: 128000,
   },
 };
